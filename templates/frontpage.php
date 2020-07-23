0a563a2d0e03bd62717f0b89424dff6030df85da
JerryKaura <jerimkaura001@gmail.com>
2020-07-23 20:03:26 +0300

    added bootstrap to frontpage

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -1,1 +1,52 @@
-<?php echo $title?>
+<?php include 'inc/header.php'?>
+
+<div class="container-flui full-width-div">
+    <div class="jumbotron">
+        <h1>MEET TO GET A JOB</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit molestias voluptas dolorum perferendis animi, amet placeat beatae architecto tempore labore, porro debitis est numquam voluptates error excepturi reiciendis maxime atque repellendus delectus aliquid! Reprehenderit, sunt voluptatem. Cum architecto recusandae natus ad iusto fuga, ipsum odit repellendus debitis iure officia commodi. Doloremque facere eveniet laborum ullam quas, fuga non soluta sint voluptatem, beatae exercitationem? Voluptas expedita autem omnis ducimus, assumenda provident ratione incidunt aspernatur vitae! Nesciunt similique at vero! Architecto consequatur, magni quidem consequuntur explicabo recusandae odio distinctio voluptas nisi eaque sint cum rerum non iste dolor aut numquam tempore rem.</p>
+    </div>
+
+    <div class="row no-gutters">
+        <div class="col-sm-3 pr-md-1">
+            <div class="card bg-dark text-white">
+                <img class="card-img" src="home-office-336378_1920.jpg" alt="Card image">
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text">Last updated 3 mins ago</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3 pr-md-1">
+            <div class="card bg-dark text-white">
+                <img class="card-img" src="home-office-336378_1920.jpg" alt="Card image">
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text">Last updated 3 mins ago</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3 pr-md-1">
+            <div class="card bg-dark text-white">
+                <img class="card-img" src="home-office-336378_1920.jpg" alt="Card image">
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text">Last updated 3 mins ago</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3 pr-md-1">
+            <div class="card bg-dark text-white">
+                <img class="card-img" src="home-office-336378_1920.jpg" alt="Card image">
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text">Last updated 3 mins ago</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'inc/footer.php'?>
